89b88f73a4b19d934ca017856a03a9e8730c4e5e
Alfredo Melero <alfredo.melero@gmail.com>
2026-07-29 15:23:33 +0200

    agregar loader de ejemplo

---
+++ loader_example.bat
@@ -0,0 +1,11 @@
+@echo off
+set ROM_PATH=%~1
+set ROM_NAME=%~n1
+
+REM 1. Change to RetroArch directory and run the game
+cd /d "C:\AttractModePlus\retroarch"
+"C:\AttractModePlus\retroarch\retroarch.exe" -L cores\fbneo_libretro.dll "%ROM_PATH%"
+
+REM 2. Once RetroArch is closed, go to ScoreBridge and upload the record
+cd /d "C:\AttractModePlus\ScoreBridge"
+python scorebridge.py %ROM_NAME%
